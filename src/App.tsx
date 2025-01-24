b80cd6ffb98bd24a2fd791da8adae63329885512
LoganHenig <62856313+LoganHenig@users.noreply.github.com>
2025-01-24 15:01:15 -0500

    added 404 not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import { PrimeReactProvider } from 'primereact/api';
 
 import { Landing } from './pages/landing/landing'
 import { Search } from './pages/search/search'
 import { Product } from './pages/product/product'
 import "primereact/resources/themes/lara-light-teal/theme.css"
+import { NotFound } from './pages/notFound/notFound';
 
 export const App = () => {
   const config = {
@@ -18,7 +19,7 @@ export const App = () => {
           <Route path='/' element={<Landing />} />
           <Route path='/search' element={<Search />} />
           <Route path='/product:id' element={<Product />} />
-          <Route path='*' element= { <div>tests</div>} />
+          <Route path='*' element= {<NotFound />}/>
         </Routes>
       </BrowserRouter>
     </PrimeReactProvider>
---
+++ src/pages/notFound/notFound.tsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+export const NotFound = () =>{
+    const navigate = useNavigate();
+    return (
+    <>
+        <h1>Hello, World! Theres nothing here</h1>
+        <button onClick={ ()=>{navigate('/product/123444')} } />
+    </>
+    )
+}

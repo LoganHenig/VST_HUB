0a5c91e0480b453bdb7be8b990fd0ad99cecc85f
LoganHenig <62856313+LoganHenig@users.noreply.github.com>
2025-01-25 16:12:43 -0500

    Installed tailwind and set up a basic blue color palette

---
+++ src/components/card.tsx
@@ -0,0 +1,12 @@
+import { Card } from "primereact/card"
+
+export const VstCard = () =>{
+    return(
+    <Card title="Advanced Card" subTitle="Card subtitle" footer={"fooot"} header={"Header"} className="md:w-25rem">
+        <p className="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+            numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+        </p>
+    </Card>
+    )
+}

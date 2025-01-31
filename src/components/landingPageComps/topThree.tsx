d1fe85daa31e5989c3219c353c78284be91350a0
Kyle <kaperont@gmail.com>
2025-01-30 23:39:06 -0500

    Use "product" as prop instead of product attributes

--- src/components/landingPageComps/topThree.tsx
+++ src/components/landingPageComps/topThree.tsx
@@ -1,7 +1,41 @@
-import { VstCard } from "../card";
+import { useState } from "react";
 import { ProductCard } from "../pcard";
 import "./landingComp.css";
 export const TopThree = () => {
+  const [topThree] = useState([
+    {
+      id: 10501,
+      title: "Kick Ninja",
+      category: "Drum Synth",
+      author: "The Him DSP",
+      description:
+        "Enter a freshly updated and thoughtfully furnished peaceful home surrounded by ancient trees, stone walls, and open meadows.",
+      rating: 5,
+    },
+    {
+      id: 10502,
+      title: "Egoist",
+      category: "Groovebox",
+      author: "Sugar Bytes",
+      description:
+        "Enter a freshly updated and thoughtfully furnished peaceful home surrounded by ancient trees, stone walls, and open meadows.",
+      rating: 4,
+    },
+    {
+      id: 10503,
+      title: "H3000 Factory Mk II",
+      category: "Harmonizer",
+      author: "Eventide",
+      description:
+        "Enter a freshly updated and thoughtfully furnished peaceful home surrounded by ancient trees, stone walls, and open meadows.",
+      rating: 3,
+    },
+  ]);
+
+  const topThreeCards = topThree.map((product) => (
+    <ProductCard product={product} />
+  ));
+
   return (
     <>
       <div className="w-screen flex justify-center min-h-20 items-center flex-col top-neg-20">
@@ -9,30 +43,7 @@ export const TopThree = () => {
           Featured Plugins
         </div>
         <div className="flex items-center justify-center space-x-10 py-10 px-20">
-          <ProductCard
-            id={10501}
-            title="Drum Synth"
-            category="Synthesizer"
-            description="Enter a freshly updated and thoughtfully furnished peaceful home
-          surrounded by ancient trees, stone walls, and open meadows."
-            rating={5}
-          />
-          <ProductCard
-            id={10502}
-            title="Drum Synth"
-            category="Synthesizer"
-            description="Enter a freshly updated and thoughtfully furnished peaceful home
-          surrounded by ancient trees, stone walls, and open meadows."
-            rating={3}
-          />
-          <ProductCard
-            id={10503}
-            title="Drum Synth"
-            category="Synthesizer"
-            description="Enter a freshly updated and thoughtfully furnished peaceful home
-          surrounded by ancient trees, stone walls, and open meadows."
-            rating={4}
-          />
+          {topThreeCards}
         </div>
       </div>
     </>
--- src/components/pcard.tsx
+++ src/components/pcard.tsx
@@ -4,31 +4,30 @@ import { Button } from "primereact/button";
 import { Rating } from "primereact/rating";
 import { useNavigate } from "react-router";
 
-export const ProductCard = ({ id, title, category, description, rating }) => {
+export const ProductCard = ({ product }) => {
   const navigate = useNavigate();
 
-  const header = <img alt={title} src="\src\assets\headphones.webp" />;
+  const header = <img alt={product.title} src="\src\assets\headphones.webp" />;
   const footer = (
     <Button
       label="View Product"
       icon="pi pi-check"
       className="w-full"
       onClick={() => {
-        navigate(`/product/${id}`);
+        navigate(`/product/${product.id}`);
       }}
     />
   );
 
   return (
     <Card
-      title={title}
-      subTitle={category}
+      title={product.title}
+      subTitle={`${product.category} by ${product.author}`}
       footer={footer}
       header={header}
-      //   className="md:w-25rem"
     >
-      <Rating value={rating} readOnly cancel={false} className="mb-5" />
-      <p className="m-0">{description}</p>
+      <Rating value={product.rating} readOnly cancel={false} className="mb-5" />
+      <p className="m-0">{product.description}</p>
     </Card>
   );
 };

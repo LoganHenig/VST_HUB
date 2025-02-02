70831a54632fbcb7191d4171a6db8ffb3211a3f1
LoganHenig <62856313+LoganHenig@users.noreply.github.com>
2025-02-01 20:58:50 -0500

    added theme context and started landing page css for light/dark mode

--- src/components/landingPageComps/hero.tsx
+++ src/components/landingPageComps/hero.tsx
@@ -1,24 +1,26 @@
+import { useTheme } from "../../context/themeContext";
 import "./landingComp.css";
 export const Hero = () => {
+  const { darkMode } = useTheme();
   return (
     <>
-      <div className="relative w-screen overflow-x-hidden flex justify-center">
+      <div className={`${ darkMode ? 'bg-hero-dark' :'bg-hero-light'} relative w-screen overflow-x-hidden flex justify-center`}>
         <div
           id="video-frame"
-          className=" relative z-10 video-width overflow-hidden rounded-lg white flex justify-center"
+          className=" relative z-10 video-width overflow-hidden rounded-lg flex justify-center"
         >
-          <div className="VST-HUB-heading text-7xl z-40 bangers-font flex flex-col invert-color">
+          <div className={`${ darkMode ? 'text-black' :'text-white'} VST-HUB-heading text-7xl z-80 bangers-font flex flex-col invert-color`}>
             <p>
               <b>VST HUB</b>
             </p>
             <div className="">FIND YOUR SOUND</div>
             {/* ADD GET STARTED BUTTON */}
           </div>
-          <div className=" width-100 overflow-x-hidden video-clip-path min-w-3xl lg:m-10 md:m-8 m-0 bg-white flex justify-center items-center">
+          <div className={`${ darkMode ? 'bg-white' : 'bg-vst-blue-900'} width-100 overflow-x-hidden video-clip-path min-w-3xl lg:m-10 md:m-8 m-0 flex justify-center items-center`}>
             <video
               muted
               loop
-              className="video-99 video-clip-path z-30"
+              className="video-99 video-clip-path z-10"
               autoPlay
             >
               <source src="src\assets\audioVideo.mp4" type="video/mp4" />

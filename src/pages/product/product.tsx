049616aa6831f8f34d0866c7e55f23921fab4b7f
LoganHenig <62856313+LoganHenig@users.noreply.github.com>
2025-02-15 20:03:01 -0500

    used prettier on product.tsx

--- src/pages/product/product.tsx
+++ src/pages/product/product.tsx
@@ -3,28 +3,30 @@ import { useEffect } from "react";
 import { ImageAndDiscription } from "../../components/productPageComps/imageAndDiscription";
 import { ProductTabSelect } from "../../components/productPageComps/productTabSelect";
 import { useAppDispatch } from "../../utils/hooks";
-import { setCommentsInStore, setId } from "../../utils/reduxSlices/productSlice";
+import {
+  setCommentsInStore,
+  setId,
+} from "../../utils/reduxSlices/productSlice";
 
 export const Product = () => {
   const dispatch = useAppDispatch();
 
   const { id } = useParams();
   useEffect(() => {
-    if(id){
-      
-      dispatch(setCommentsInStore([]))
-      dispatch(setId(id))
+    if (id) {
+      dispatch(setCommentsInStore([]));
+      dispatch(setId(id));
     }
-  },[id])
+  }, [id]);
 
   return (
     <>
-      {id &&
-          <div className="flex flex-col w-screen justify-center items-center">
-            <ImageAndDiscription id={id}/>
-            <ProductTabSelect/>
-          </div>
-      }
+      {id && (
+        <div className="flex flex-col w-screen justify-center items-center">
+          <ImageAndDiscription id={id} />
+          <ProductTabSelect />
+        </div>
+      )}
     </>
   );
 };
